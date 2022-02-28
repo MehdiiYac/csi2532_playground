e19df878a055e0098bcafebc10b02244be503d08
MehdiiYac <91090403+MehdiiYac@users.noreply.github.com>
2022-02-28 10:44:23 -0500

    [ADD] sql files "schema" & "seed"
    Lab 6

--- seed.sql
+++ seed.sql
@@ -1,28 +1,100 @@
+INSERT INTO customers (id, name, address, amount)
+VALUES (1, 'John', 'Ottawa', 8.5),
+    (2, 'Amy', 'Orleans', 9.0),
+    (3, 'Peter', 'Gatineau', 6.3);
+INSERT INTO artists (name, birthplace, style, dateofbirth)
+VALUES ('Caravaggio', 'Milan', 'Baroque', '1571-09-28'),
+    ('Smith', 'Ottawa', 'Modern', '1977-12-12'),
+    ('Picasso', 'Malaga', 'Cubism', '1881-10-25');
+INSERT INTO artworks (title, year, type, price, artist_name)
+VALUES ('Blue', 2000, 'Modern', 10000.00, 'Smith'),
+    (
+        'The Cardsharps',
+        1594,
+        'Baroque',
+        40000.00,
+        'Caravaggio'
+    );
+UPDATE customers
+SET amount = 9.8
+WHERE address = 'Gatineau';
+DELETE FROM customers
+WHERE name = 'John';
+INSERT INTO customers (id, name, address, amount, rating)
+VALUES (4, 'Emre', 'Preston', 20000.00, 5),
+    (5, 'Saeid', null, 40000.00, 6);
+INSERT INTO artists (name, birthplace, style, dateofbirth, country)
+VALUES (
+        'Leonardo',
+        'Florence',
+        'Renaissance',
+        '04-15-1452',
+        'Italy'
+    ),
+    (
+        'Michelangelo',
+        'Arezzo',
+        'Renaissance',
+        '03-06-1475',
+        'Italy'
+    ),
+    (
+        'Josefa',
+        'Seville',
+        'Baroque',
+        '09-09-1630',
+        'Spain'
+    ),
+    (
+        'Hans Hofmann',
+        'Weisenburg',
+        'Modern',
+        '02-17-1966',
+        'Germany'
+    ),
+    (
+        'John',
+        'San Francisco',
+        'Modern',
+        '02-17-1920',
+        'USA'
+    );
+INSERT INTO artworks (title, year, type, price, artist_name)
+VALUES ('Waves', 2000, null, 4000.00, 'Smith'),
+    (
+        'Three Musicians',
+        1921,
+        'Modern',
+        11000.00,
+        'Picasso'
+    )
 
-INSERT INTO department
-  (dept_name, building, budget)
-VALUES
-  ('Biology', 'Watson', 90000),
-  ('Comp. Sci.', 'Taylor', 100000),
-  ('Elec. Eng.', 'Taylor', 85000),
-  ('Finance', 'Painter', 120000),
-  ('History', 'Painter', 50000),
-  ('Music', 'Packard', 80000),
-  ('Physics', 'Watson', 70000);
 
-INSERT INTO course
-  (course_id, title, dept_name, credits)
-VALUES
-  ('BIO-101', 'Intro. to Biology', 'Biology', 4),
-  ('BIO-301', 'Genetics', 'Biology', 4),
-  ('BIO-399', 'Computational Biology', 'Biology', 3),
-  ('CS-101', 'Intro. to Computer Science', 'Comp. Sci.', 4),
-  ('CS-190', 'Game Design', 'Comp. Sci.', 4),
-  ('CS-315', 'Robotics', 'Comp. Sci.', 3),
-  ('CS-319', 'Image Processing', 'Comp. Sci.', 3),
-  ('CS-347', 'Database System Concepts', 'Comp. Sci.', 3),
-  ('EE-181', 'Intro. to Digital Systems', 'Elec. Eng.', 3),
-  ('FIN-201', 'Investment Banking', 'Finance', 3),
-  ('HIS-351', 'World History', 'History', 3),
-  ('MU-199', 'Music Video Production', 'Music', 3),
-  ('PHY-101', 'Physical Principles', 'Physics', 4);
+-- Question 1:
+SELECT name,birthplace FROM artists;
+DECLARE @Space varchar(20)
+SELECT @Space = '\n';
+SELECT @Space;
+-- Question 2:
+SELECT title,price FROM artworks WHERE year > 1600;
+SELECT @Space;
+-- Question 3:
+SELECT title,type FROM artworks WHERE year = 2000 OR artist_name = 'Picasso';
+SELECT @Space;
+-- Question 4:
+SELECT name,birthplace FROM artist WHERE EXTRACT(YEAR FROM dateofbirth) < 1930 OR EXTRACT(YEAR FROM dateofbirth) > 1880;
+SELECT @Space;
+-- Question 5:
+SELECT name,country FROM artists WHERE style IN ('Baroque', 'Modern', 'Renaissance');
+SELECT @Space;
+-- Question 6:
+SELECT * FROM artworks ORDER BY title;
+SELECT @Space;
+--Question 7:
+SELECT name,id FROM customers JOIN likeartists ON customers.id = likeartists.customer_id WHERE artist_name = 'Picasso';
+SELECT @Space;
+-- Question 8:
+SELECT name FROM customers JOIN likedartists ON customers.id = likeartists.customer_id JOIN artists ON artists.name = likeartists.artist_name WHERE artists.style = 'Renaissance' AND
+(artists JOIN artworks ON artists.name = artworks.artist_name WHERE price > 30000);
+
+
